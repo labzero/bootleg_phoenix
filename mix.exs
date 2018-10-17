aec8ae05295e56516ef37f4e5c7fe7ae56944345
Billy Forester <william.forester@gmail.com>
2018-10-17 11:46:31 -0500

    move to elixir 1.5 for deps. Also upgraded deps to newer versions

--- mix.exs
+++ mix.exs
@@ -5,19 +5,20 @@ defmodule BootlegPhoenix.Mixfile do
   @source "https://github.com/labzero/bootleg_phoenix"
 
   def project do
-    [app: :bootleg_phoenix,
-     version: @version,
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     elixirc_paths: elixirc_paths(Mix.env),
-     docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
-     test_coverage: [tool: ExCoveralls],
-     dialyzer: [plt_add_deps: :transitive, plt_add_apps: [:mix, :sshkit]],
-     package: package(),
-     description: description(),
-   ]
+    [
+      app: :bootleg_phoenix,
+      version: @version,
+      elixir: "~> 1.5",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [plt_add_deps: :transitive, plt_add_apps: [:mix, :sshkit]],
+      package: package(),
+      description: description()
+    ]
   end
 
   # Configuration for the OTP application
@@ -39,14 +40,14 @@ defmodule BootlegPhoenix.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:bootleg, "~> 0.5", runtime: false},
-      {:credo, "~> 0.7", only: [:dev, :test]},
+      {:bootleg, "~> 0.7.0", runtime: false},
+      {:credo, "~> 0.8", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.6", only: :test},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:mock, "~> 0.2.0", only: :test},
-      {:junit_formatter, "~> 2.0", only: :test},
-      {:temp, "~> 0.4.3", only: :test}
+      {:junit_formatter, "~> 2.2", only: :test},
+      {:temp, "~> 0.4.7", only: :test}
     ]
   end
 
@@ -55,12 +56,14 @@ defmodule BootlegPhoenix.Mixfile do
   end
 
   defp package do
-    [maintainers: ["labzero", "Brien Wankel", "Ned Holets", "Rob Adams"],
-     licenses: ["MIT"],
-     links: %{
-      "GitHub" => @source,
-      "Bootleg" => "https://github.com/labzero/bootleg"
-    }]
+    [
+      maintainers: ["labzero", "Brien Wankel", "Ned Holets", "Rob Adams"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source,
+        "Bootleg" => "https://github.com/labzero/bootleg"
+      }
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
